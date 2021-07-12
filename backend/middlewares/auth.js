b0fbd68674701f316df9a93f37cdf8f7b9508053
ulist2020 <75067604+ulist2020@users.noreply.github.com>
2021-07-12 21:41:27 +0500

    Update auth.js

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -13,7 +13,7 @@ module.exports = (req, res, next) => {
   let payload;
   try {
     // попытаемся верифицировать токен
-    payload = jwt.verify(token, NODE_ENV !== 'production' ? JWT_SECRET : 'some-secret-key');
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key');
   } catch (err) {
     throw new UnauthorizedError('Необходима авторизация');
   }
