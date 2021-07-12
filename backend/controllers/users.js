3fa69f315a7e832241586973b56506a2ec09d5f9
ulist2020 <75067604+ulist2020@users.noreply.github.com>
2021-07-12 21:43:09 +0500

    Update users.js

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -123,7 +123,7 @@ module.exports.login = (req, res, next) => {
   return User.findUserByCredentials(email, password)
     .then((user) => {
       // создадим токен
-      const token = jwt.sign({ _id: user._id }, NODE_ENV !== 'production' ? JWT_SECRET : 'some-secret-key', { expiresIn: '7d' });
+      const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key', { expiresIn: '7d' });
       // вернём токен
       res.send({ token });
     })
